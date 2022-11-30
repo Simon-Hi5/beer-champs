63eece2a93502aec3168c8caca0d178af15c3fb6
Simon <simon.hirner99@gmail.com>
2022-11-30 10:08:52 +0100

    Setup Spring Boot project.

---
+++ src/test/java/at/ac/uibk/beerchamps/BeerchampsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.uibk.beerchamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerchampsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
