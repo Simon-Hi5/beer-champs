72a9bbbdd001dfc44b41c72e9c38c4eef62c77c9
wolfgangvigl <wolfgang.vigl@gmail.com>
2022-12-03 12:47:39 +0100

    Create Repository for Tournament

---
+++ src/main/java/at/ac/uibk/beerchamps/repository/TournamentRepository.java
@@ -0,0 +1,7 @@
+package at.ac.uibk.beerchamps.repository;
+
+import at.ac.uibk.beerchamps.persistence.Tournament;
+
+public interface TournamentRepository extends AbstractRepository<Tournament, Long> {
+
+}
