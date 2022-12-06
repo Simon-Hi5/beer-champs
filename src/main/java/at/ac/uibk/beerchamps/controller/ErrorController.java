c22e540dd3e79586e1c3f50ff6acdebb1eca458a
Simon Hirner <Simon.Hirner@student.uibk.ac.at>
2022-12-06 23:27:51 +0100

    Add error handling.

---
+++ src/main/java/at/ac/uibk/beerchamps/controller/ErrorController.java
@@ -0,0 +1,31 @@
+package at.ac.uibk.beerchamps.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Calendar;
+import java.util.Date;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object errorCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        int code = Integer.parseInt(errorCode.toString());
+        Calendar calendar = Calendar.getInstance(request.getLocale());
+        Date timestamp = calendar.getTime();
+
+        model.addAttribute("status", errorCode);
+        model.addAttribute("error", HttpStatus.valueOf(code));
+        model.addAttribute("timestamp", timestamp);
+        model.addAttribute("message", request.getAttribute("javax.servlet.error.exception"));
+        model.addAttribute("path", request.getAttribute("javax.servlet.forward.request_uri"));
+
+        return "error";
+    }
+}
