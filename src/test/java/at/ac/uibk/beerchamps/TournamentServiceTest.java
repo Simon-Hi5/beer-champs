2337b740fa11c3fd2a1cbd7d91b7d51badbf73d4
Simon Hirner <Simon.Hirner@student.uibk.ac.at>
2023-02-02 12:59:40 +0100

    Add repository, service and controller tests.

--- src/test/java/at/ac/uibk/beerchamps/TournamentServiceTest.java
+++ src/test/java/at/ac/uibk/beerchamps/TournamentServiceTest.java
@@ -1,6 +1,8 @@
 package at.ac.uibk.beerchamps;
 
+import at.ac.uibk.beerchamps.persistence.Team;
 import at.ac.uibk.beerchamps.persistence.Tournament;
+import at.ac.uibk.beerchamps.persistence.TournamentType;
 import at.ac.uibk.beerchamps.repository.TournamentRepository;
 import at.ac.uibk.beerchamps.service.TournamentService;
 import at.ac.uibk.beerchamps.service.TournamentServiceImpl;
@@ -14,6 +16,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertThrows;
 
@@ -27,7 +31,7 @@ class TournamentServiceTest {
     TournamentService tournamentService = new TournamentServiceImpl();
 
     @Test
-    void createTournamentSuccess() {
+    void createTournament() {
         ArgumentCaptor<Tournament> tournamentCaptor = ArgumentCaptor.forClass(Tournament.class);
         Tournament newTournament = new Tournament();
         tournamentService.createTournament(newTournament);
@@ -40,6 +44,46 @@ class TournamentServiceTest {
         assertThrows(EntityNotFoundException.class, () -> {
             tournamentService.findTournament(5L);
         });
+    }
+
+    @Test
+    void addTeamsToTournament() {
+        ArgumentCaptor<Tournament> tournamentCaptor = ArgumentCaptor.forClass(Tournament.class);
+
+        Tournament t = new Tournament();
+        Team t1 = new Team();
+        t1.setTeamName("Team1");
+        Team t2 = new Team();
+        t2.setTeamName("Team2");
+        List<Team> teamList = new ArrayList<>();
+        teamList.add(t1);
+        teamList.add(t2);
+        t.setTeams(teamList);
+
+        tournamentService.createTournament(t);
+        Mockito.verify(tournamentRepository).save(tournamentCaptor.capture());
+
+        Assertions.assertEquals(t1, tournamentCaptor.getValue().getTeams().get(0));
+        Assertions.assertEquals(t2, tournamentCaptor.getValue().getTeams().get(1));
+    }
+
+    @Test
+    void addNameToTournament() {
+        ArgumentCaptor<Tournament> tournamentCaptor = ArgumentCaptor.forClass(Tournament.class);
+
+        String tournamentName = "Tournament Name";
+        String hostName = "Host Name";
+
+        Tournament t = new Tournament();
+        t.setTournamentName(tournamentName);
+        t.setTournamentType(TournamentType.ROUNDROBIN);
+        t.setHostName(hostName);
+
+        tournamentService.createTournament(t);
+        Mockito.verify(tournamentRepository).save(tournamentCaptor.capture());
+
+        Assertions.assertEquals(tournamentName, tournamentCaptor.getValue().getTournamentName());
+        Assertions.assertEquals(hostName, tournamentCaptor.getValue().getHostName());
     }
 
 }
