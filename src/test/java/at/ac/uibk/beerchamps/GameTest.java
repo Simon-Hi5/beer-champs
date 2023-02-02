0a4db760b7778ed59e65851c7d5f5e341bf74bc3
BenDai <benjamin.daiss@student.uibk.ac.at>
2023-02-02 13:01:04 +0100

    Added UnitTests for Game and Team Class

---
+++ src/test/java/at/ac/uibk/beerchamps/GameTest.java
@@ -0,0 +1,108 @@
+package at.ac.uibk.beerchamps;
+
+import at.ac.uibk.beerchamps.persistence.Game;
+import at.ac.uibk.beerchamps.persistence.Player;
+import at.ac.uibk.beerchamps.persistence.Team;
+import at.ac.uibk.beerchamps.persistence.Tournament;
+import at.ac.uibk.beerchamps.repository.TeamRepository;
+import at.ac.uibk.beerchamps.repository.TournamentRepository;
+import at.ac.uibk.beerchamps.service.TeamService;
+import at.ac.uibk.beerchamps.service.TeamServiceImpl;
+import at.ac.uibk.beerchamps.service.TournamentService;
+import at.ac.uibk.beerchamps.service.TournamentServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GameTest {
+    @Mock
+    TournamentRepository tournamentRepository;
+    @Mock
+    TeamRepository teamRepository;
+    @InjectMocks
+    TournamentService tournamentService = new TournamentServiceImpl();
+    @InjectMocks
+    TeamService teamService = new TeamServiceImpl();
+    Tournament newTournament = new Tournament();
+    Player p1,p2,p3,p4,p5,p6,p7,p8;
+    HashSet<Player> set1,set2,set3,set4;
+    Team t1,t2,t3,t4;
+
+    Game g1,g2;
+    @BeforeEach
+    public void init(){
+        tournamentService.createTournament(newTournament);
+
+        p1 = new Player("p1");
+        p2 = new Player("p2");
+        p3 = new Player("p3");
+        p4 = new Player("p4");
+        p5 = new Player("p5");
+        p6 = new Player("p6");
+        p7 = new Player("p7");
+        p8 = new Player("p8");
+
+        set1 = new HashSet<>();
+        set2 = new HashSet<>();
+        set3 = new HashSet<>();
+        set4 = new HashSet<>();
+
+        set1.add(p1);
+        set1.add(p2);
+        set2.add(p3);
+        set2.add(p4);
+        set3.add(p5);
+        set3.add(p6);
+        set4.add(p7);
+        set4.add(p8);
+
+        t1 = new Team(set1,"team1",newTournament);
+        t2 = new Team(set2,"team2",newTournament);
+        t3 = new Team(set3,"team3",newTournament);
+        t4 = new Team(set4,"team4",newTournament);
+
+
+        g1 = new Game();
+        g2 = new Game();
+    }
+    @Test
+    public void SetTeams(){
+        Assertions.assertEquals(null,g1.getTeam1());
+
+        g1.setTeam1(t1);
+        g1.setTeam2(t2);
+
+        Assertions.assertEquals(t1,g1.getTeam1());
+        Assertions.assertEquals(t2,g1.getTeam2());
+
+    }
+    @Test
+    public void SetWinner(){
+        Assertions.assertEquals(null,g1.getWinner());
+
+        g1.setWinner(t2);
+
+        Assertions.assertEquals(t2, g1.getWinner());
+
+    }
+
+    @Test
+    public void GameHash(){
+
+        g1.setId(999);
+        g2.setId(12234);
+
+        Assertions.assertEquals(Objects.hash(999),g1.hashCode());
+        Assertions.assertEquals(Objects.hash(12234),g2.hashCode());
+    }
+}
--- src/test/java/at/ac/uibk/beerchamps/TournamentServiceTest.java
+++ src/test/java/at/ac/uibk/beerchamps/TournamentServiceTest.java
@@ -1,7 +1,12 @@
 package at.ac.uibk.beerchamps;
 
+import at.ac.uibk.beerchamps.persistence.Player;
+import at.ac.uibk.beerchamps.persistence.Team;
 import at.ac.uibk.beerchamps.persistence.Tournament;
+import at.ac.uibk.beerchamps.repository.TeamRepository;
 import at.ac.uibk.beerchamps.repository.TournamentRepository;
+import at.ac.uibk.beerchamps.service.TeamService;
+import at.ac.uibk.beerchamps.service.TeamServiceImpl;
 import at.ac.uibk.beerchamps.service.TournamentService;
 import at.ac.uibk.beerchamps.service.TournamentServiceImpl;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +20,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,10 +31,14 @@ class TournamentServiceTest {
 
     @Mock
     TournamentRepository tournamentRepository;
+    @Mock
+    TeamRepository teamRepository;
 
     @InjectMocks
     TournamentService tournamentService = new TournamentServiceImpl();
 
+    @InjectMocks
+    TeamService teamService = new TeamServiceImpl();
     @Test
     void createTournamentSuccess() {
         ArgumentCaptor<Tournament> tournamentCaptor = ArgumentCaptor.forClass(Tournament.class);
@@ -40,6 +53,37 @@ class TournamentServiceTest {
         assertThrows(EntityNotFoundException.class, () -> {
             tournamentService.findTournament(5L);
         });
+    }
+
+    @Test
+    void generateGames(){
+        ArgumentCaptor<Tournament> tournamentCaptor = ArgumentCaptor.forClass(Tournament.class);
+        Tournament newTournament = new Tournament();
+        tournamentService.createTournament(newTournament);
+
+        HashSet<Player> set1 = new HashSet<>();
+        HashSet<Player> set2 = new HashSet<>();
+
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        Player p3 = new Player("p3");
+        Player p4 = new Player("p4");
+
+        set1.add(p1);
+        set1.add(p2);
+        set2.add(p3);
+        set2.add(p4);
+
+        Team t1 = new Team(set1,"team1",newTournament);
+        Team t2 = new Team(set2,"team2",newTournament);
+        teamService.createTeam(t1, newTournament.getId());
+        teamService.createTeam(t2, newTournament.getId());
+
+        teamRepository.save(t1);
+        teamRepository.save(t2);
+        tournamentRepository.save(newTournament);
+
+        Assertions.assertEquals(2,newTournament.getTeams().size());
     }
 
 }
